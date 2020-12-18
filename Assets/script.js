f036210c348bacf1d18bc577a40f43121fec01df
Ryan <ryanpburnett@yahoo.com>
2020-12-17 22:28:43 -0600

    first commit

---
+++ Assets/script.js
@@ -0,0 +1,9 @@
+var weatherQueryURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=41.8781&lon=87.6298&exclude={part}&appid=6e365be137d7d25f10a10cd49612b3b5';
+
+var weather = $('#weather');
+
+$.ajax({
+    url: weatherQueryURL,
+    method: "GET"
+}).then(function(response) {
+    weather.attr('src', 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=6e365be137d7d25f10a10cd49612b3b5');
