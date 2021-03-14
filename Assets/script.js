c8992d9ace322a72b6f69bcad02e054f90fbfb3d
Ryan <ryanpburnett@yahoo.com>
2021-03-14 16:01:36 -0500

    pulling in image, not yet displaying

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,9 @@
 const apiID = '476bb8ed1264e5d0ca7169cf86193cf6';
 
 let weatherHTML = document.getElementById("weather");
-let city = document.getElementById("city")
-let button = document.getElementById("button")
+let icon = document.getElementById("icon")
+let city = document.getElementById("city");
+let button = document.getElementById("button");
 
 let weatherQueryURL = `https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=${apiID}&units=imperial`
 
@@ -11,9 +12,15 @@ function getWeather() {
         url: weatherQueryURL,
         method: "GET"
     }).then(function(response) {
+        let icon = response.weather[0].icon
+        let iconURL = "https://openweathermap.org/img/wn/" +
+        icon + ".png"
         console.log(response)
-        weatherHTML.innerText = "current temperature: " + response.main.temp
+        weatherHTML.innerText = 
+        `current temperature: ${response.main.temp}`
+        icon.innerText = `<img src="${iconURL}>"`
+        console.log(iconURL)
+        console.log(icon)
     })
 }
 
-// `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid${apiID}`
