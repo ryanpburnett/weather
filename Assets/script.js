8aa25386c485d0a47e62af4d869bbbf70bbf5459
Ryan <ryanpburnett@yahoo.com>
2021-03-14 21:20:11 -0500

    added humidity and wind speed

--- Assets/script.js
+++ Assets/script.js
@@ -2,7 +2,9 @@ const apiID = '476bb8ed1264e5d0ca7169cf86193cf6';
 
 let date = document.getElementById("date")
 let weatherHTML = document.getElementById("weather");
-let iconDiv = document.getElementById("icon-div")
+let iconDiv = document.getElementById("icon-div");
+let humidity = document.getElementById("humidity");
+let wind = document.getElementById("wind");
 let city = document.getElementById("city");
 let button = document.getElementById("button");
 
@@ -16,18 +18,23 @@ function getWeather() {
         let icon = response.weather[0].icon
         let iconURL = "https://openweathermap.org/img/wn/" +
         icon + ".png"
-        console.log(response)
+        
         date.innerText = new Date()
         weatherHTML.innerText = 
         `Current temperature in ${city.value}: 
-        ${response.main.temp} °F`
+        ${response.main.temp} °F.`
         iconDiv.innerHTML = `<img src="${iconURL}">`
+        humidity.innerText = 
+        `Humidity is ${response.main.humidity}%.`
+        wind.innerText = 
+        `Wind speed is ${response.wind.speed} mph.`
 
         if (icon.includes("d")) {
             document.body.setAttribute("class", "day")
         }else if (icon.includes("n")) {
             document.body.setAttribute("class", "night")
         }
+        console.log(response)
     })
     
 }
